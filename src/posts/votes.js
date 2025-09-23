5eb78f9468c060325af871621983b5da63f237f9
Jordan Bain <jnbain@andrew.cmu.edu>
2025-09-23 15:08:56 +0000

    Revert "fixed upvote functionality in votes.js"
    This reverts commit d73cc7cb7bdb67ad6a0cf4e73d9185d6846c5770.

--- src/posts/votes.js
+++ src/posts/votes.js
@@ -193,25 +193,12 @@ module.exports = function (Posts) {
 		const postData = await Posts.getPostFields(pid, ['pid', 'uid', 'tid']);
 		const newReputation = await user.incrementUserReputationBy(postData.uid, type === 'upvote' ? 1 : -1);
 
-		let delta = 0;
-		if (unvote) {
-			if (voteStatus.upvoted) {
-				delta = -1;
-			}
-		} else if (type === 'upvote') {
-			if (!voteStatus.upvoted) {
-				delta = 1;
-			}
-		} else if (type === 'downvote') {
-			if (voteStatus.upvoted) {
-				delta = -1;
-			}
-		}
-
-		if (delta !== 0 && postData && postData.uid) {
+		if (type === 'upvote' && !unvote && !voteStatus.upvoted && postData && postData.uid) {
 			try {
-				await helpfulness.increment(postData.uid, delta);
-			} catch (err) {}
+				await helpfulness.increment(postData.uid, 1);
+			} catch (err) {
+				// intentionally non-fatal; do not block voting if helpfulness write fails
+			}
 		}
 
 
