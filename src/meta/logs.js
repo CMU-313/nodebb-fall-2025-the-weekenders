188346b9fbb75e74f7961d4912ce5f83fc4a1d8c
Jordan Bain <jnbain@andrew.cmu.edu>
2025-09-24 18:36:31 +0000

    testing lint fix

--- src/meta/logs.js
+++ src/meta/logs.js
@@ -1,3 +1,20 @@
+// 'use strict';
+
+// const path = require('path');
+// const fs = require('fs');
+
+// const Logs = module.exports;
+
+// Logs.path = path.resolve(__dirname, '../../logs/output.log');
+
+// Logs.get = async function () {
+// 	return await fs.promises.readFile(Logs.path, 'utf-8');
+// };
+
+// Logs.clear = async function () {
+// 	await fs.promises.truncate(Logs.path, 0);
+// };
+
 'use strict';
 
 const path = require('path');
@@ -8,9 +25,26 @@ const Logs = module.exports;
 Logs.path = path.resolve(__dirname, '../../logs/output.log');
 
 Logs.get = async function () {
-	return await fs.promises.readFile(Logs.path, 'utf-8');
+  try {
+    // return the log text; if file is missing, return empty string
+    return await fs.promises.readFile(Logs.path, 'utf8');
+  } catch (err) {
+    if (err && err.code === 'ENOENT') return '';
+    throw err;
+  }
 };
 
 Logs.clear = async function () {
-	await fs.promises.truncate(Logs.path, 0);
+  try {
+    await fs.promises.truncate(Logs.path, 0);
+  } catch (err) {
+    if (err && err.code === 'ENOENT') {
+      // create the logs dir/file if they don't exist
+      await fs.promises.mkdir(path.dirname(Logs.path), { recursive: true });
+      await fs.promises.writeFile(Logs.path, '', 'utf8');
+      return;
+    }
+    throw err;
+  }
 };
+
