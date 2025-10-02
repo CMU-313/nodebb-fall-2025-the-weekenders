04b2a7ef2122b302ae3fe073f27505d78509a51c
Jordan Bain <jnbain@andrew.cmu.edu>
2025-10-02 23:22:11 +0000

    feat(posts): add endorsement helper to set/clear flags

---
+++ src/posts/endorse.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const db = require('../database');
+
+// Endorse a post by setting an endorsed flag and timestamp
+async function endorse(pid) {
+	const key = `post:${pid}`;
+	const endorsedAt = Date.now();
+
+	await db.setObject(key, {
+		endorsed: 1,             
+		endorsed_at: endorsedAt, 
+	});
+
+	return {
+		pid: Number(pid),
+		endorsed: true,
+		endorsed_at: endorsedAt,
+	};
+}
+
+// Remove endorsement by clearing the flag and timestamp
+async function unendorse(pid) {
+	const key = `post:${pid}`;
+
+	await db.setObject(key, {
+		endorsed: 0,
+		endorsed_at: 0,
+	});
+
+	return {
+		pid: Number(pid),
+		endorsed: false,
+	};
+}
+
+module.exports = { endorse, unendorse };
