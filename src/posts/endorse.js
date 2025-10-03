d2a3b01cd3611bbc884eecf847a5fe33e54761b9
Jordan Bain <jnbain@andrew.cmu.edu>
2025-10-03 23:42:16 +0000

    added endorsing rank

--- src/posts/endorse.js
+++ src/posts/endorse.js
@@ -1,22 +1,23 @@
 'use strict';
 
 const db = require('../database');
-const utils = require('../utils');
 
 // Endorse a post by setting an endorsed flag and timestamp
-async function endorse(pid) {
+async function endorse(pid, rank = null) {
 	const key = `post:${pid}`;
 	const endorsedAt = Date.now();
 
 	await db.setObject(key, {
 		endorsed: 1,             
-		endorsed_at: endorsedAt, 
+		endorsed_at: endorsedAt,
+		endorsed_rank: rank || 0, 
 	});
 
 	return {
 		pid: Number(pid),
 		endorsed: true,
-		endorsed_at: utils.toISOString(endorsedAt),
+		endorsed_at: endorsedAt,  
+		endorsed_rank: rank || 0,
 	};
 }
 
@@ -26,12 +27,15 @@ async function unendorse(pid) {
 
 	await db.setObject(key, {
 		endorsed: 0,
-		endorsed_at: 0,
+		endorsed_at: null,
+		endorsed_rank: null,
 	});
 
 	return {
 		pid: Number(pid),
 		endorsed: false,
+		endorsed_at: null,
+		endorsed_rank: null,
 	};
 }
 
