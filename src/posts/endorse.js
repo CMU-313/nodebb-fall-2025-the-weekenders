f197891975bf5490aefdc25ed62ba111fb5d016e
Jordan Bain <jnbain@andrew.cmu.edu>
2025-10-04 02:05:10 +0000

    fixed functionality for endorse.js

--- src/posts/endorse.js
+++ src/posts/endorse.js
@@ -2,41 +2,41 @@
 
 const db = require('../database');
 
-// Endorse a post by setting an endorsed flag and timestamp
-async function endorse(pid, rank = null) {
-	const key = `post:${pid}`;
-	const endorsedAt = Date.now();
+module.exports = function (Posts) {
+	// Endorse a post by setting an endorsed flag and timestamp
+	Posts.endorse = async function (pid, rank = null) {
+		const key = `post:${pid}`;
+		const endorsedAt = Date.now();
 
-	await db.setObject(key, {
-		endorsed: 1,             
-		endorsed_at: endorsedAt,
-		endorsed_rank: rank || 0, 
-	});
+		await db.setObject(key, {
+			endorsed: 1,             
+			endorsed_at: endorsedAt,
+			endorsed_rank: rank || 0,
+		});
 
-	return {
-		pid: Number(pid),
-		endorsed: true,
-		endorsed_at: endorsedAt,  
-		endorsed_rank: rank || 0,
+		return {
+			pid: Number(pid),
+			endorsed: true,
+			endorsed_at: endorsedAt,
+			endorsed_rank: rank || 0,
+		};
 	};
-}
 
-// Remove endorsement by clearing the flag and timestamp
-async function unendorse(pid) {
-	const key = `post:${pid}`;
+	// Remove endorsement by clearing the flag and timestamp
+	Posts.unendorse = async function (pid) {
+		const key = `post:${pid}`;
 
-	await db.setObject(key, {
-		endorsed: 0,
-		endorsed_at: null,
-		endorsed_rank: null,
-	});
+		await db.setObject(key, {
+			endorsed: 0,
+			endorsed_at: null,
+			endorsed_rank: null,
+		});
 
-	return {
-		pid: Number(pid),
-		endorsed: false,
-		endorsed_at: null,
-		endorsed_rank: null,
+		return {
+			pid: Number(pid),
+			endorsed: false,
+			endorsed_at: null,
+			endorsed_rank: null,
+		};
 	};
-}
-
-module.exports = { endorse, unendorse };
+};
