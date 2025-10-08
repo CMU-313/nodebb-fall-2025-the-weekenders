e12f78be6241df93b178f4e42139c2598fd423b8
harsher216 <hakunuri@andrew.cmu.edu>
2025-10-08 08:11:07 +0000

    fix(client): call write namespace /v3/write/posts/:pid/endorse

--- public/src/client/topic.js
+++ public/src/client/topic.js
@@ -489,7 +489,7 @@ define('forum/topic', [
 		var pid = $a.data('pid');
 		if (!pid) { return; }
 
-		api.put('/v3/posts/' + pid + '/endorse').then(function () {
+		api.put('/v3/write/posts/' + pid + '/endorse').then(function () {
 			alerts.success('Post endorsed.');
 			// hide "Endorse", show "Unendorse" in the same dropdown
 			$a.closest('li').addClass('hidden');
@@ -505,7 +505,7 @@ define('forum/topic', [
 		var pid = $a.data('pid');
 		if (!pid) { return; }
 
-		api.del('/v3/posts/' + pid + '/endorse').then(function () {
+		api.del('/v3/write/posts/' + pid + '/endorse').then(function () {
 			alerts.success('Post unendorsed.');
 			$a.closest('li').addClass('hidden');
 			$a.closest('ul,.dropdown-menu').find('[component="post/endorse"]').closest('li').removeClass('hidden');
